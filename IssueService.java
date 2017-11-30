95b4cf2ea14d728ebfc02e0059ba332d89a3dfdb
such00bd <34118335+such00bd@users.noreply.github.com>
2017-11-30 13:57:04 +0530

    Update IssueService.java
    please review

--- IssueService.java
+++ IssueService.java
@@ -1,15 +1,31 @@
 @Service
 public class IssueService {
 
+	@Autowired
+	private IssueRepository issueRepository;
 
-   @Autowired
-   private IssueRepository issueRepository;
-   
-   public Issue update(Issue issue) {
-		
-		// TODO: Validate the issue.status first; 
-		// Status can be updated from CREATED to IN_PROGRESS to COMPLETED. 
-		
+	public Issue update(Issue issue) throws InvalidIssueStatusException, IssueUpdateException{
+
+		// TODO: Validate the issue.status first;
+		// Status can be updated from CREATED to IN_PROGRESS to COMPLETED.
+		if(issue ==  null || issueRepository == null || 
+				(getIssueStatus(issue.getStatus()) - getIssueStatus(issueRepository.getCurrentStatus(issue)) != 1)){
+			throw new InvalidIssueStatusException("Issue status is invalid!");
+		}		
+
 		// TODO: Save the issue using issueRepository
-   }
+		issueRepository.updateIssue(issue);
+	}
+	
+	private int getIssueStatus(String issueRepoStatus){
+		if("CREATED".equalsIgnoreCase(issueRepoStatus)){
+			return 1;
+		} else if("IN_PROGRESS".equalsIgnoreCase(issueRepoStatus)){
+			return 2;
+		} else if("COMPLETED".equalsIgnoreCase(issueRepoStatus)){
+			return 3;
+		} else{
+			return -10;
+		}
+	}
 }
