ee2effca09f4e6773e282ec562b8ca995a89e7a4
such00bd <34118335+such00bd@users.noreply.github.com>
2017-11-30 16:23:36 +0530

    Update IssueService.java

--- IssueService.java
+++ IssueService.java
@@ -1,31 +1,57 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 @Service
 public class IssueService {
 
 	@Autowired
 	private IssueRepository issueRepository;
 
-	public Issue update(Issue issue) throws InvalidIssueStatusException, IssueUpdateException{
+	public Issue update(Issue issue) throws InvalidIssueStatusException {
 
 		// TODO: Validate the issue.status first;
 		// Status can be updated from CREATED to IN_PROGRESS to COMPLETED.
-		if(issue ==  null || issueRepository == null || 
-				(getIssueStatus(issue.getStatus()) - getIssueStatus(issueRepository.getCurrentStatus(issue)) != 1)){
+		if(!isValidStatus(issue)){
 			throw new InvalidIssueStatusException("Issue status is invalid!");
-		}		
-
+		}
+		
 		// TODO: Save the issue using issueRepository
-		issueRepository.updateIssue(issue);
+		return issueRepository.updateIssue(issue);
 	}
 	
-	private int getIssueStatus(String issueRepoStatus){
-		if("CREATED".equalsIgnoreCase(issueRepoStatus)){
-			return 1;
-		} else if("IN_PROGRESS".equalsIgnoreCase(issueRepoStatus)){
-			return 2;
-		} else if("COMPLETED".equalsIgnoreCase(issueRepoStatus)){
-			return 3;
-		} else{
-			return -10;
+	/**
+	 * Checks if issue status is valid to update
+	 * 
+	 * @param issue
+	 * @return
+	 */
+	private boolean isValidStatus(Issue issue) {
+		try {
+			if (issue == null
+					|| issue.getStatus() == null
+					|| (IssueStatus.valueOf(issue.getStatus().toUpperCase())
+							- IssueStatus.valueOf(issueRepository.getCurrentStatus(issue).toUpperCase()) < 0)) {
+				return false;
+
+			}
+		} catch (IllegalArgumentException excep) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * 
+	 * @author suman
+	 * 
+	 */
+	enum IssueStatus {
+		CREATED(1), IN_PROGRESS(2), COMPLETED(3);
+
+		private int value;
+
+		private IssueStatus(int value) {
+			this.value = value;
 		}
 	}
 }
